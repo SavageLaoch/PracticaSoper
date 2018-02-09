28985e3e8150d8769b26f2230b97bd693aabebd0
SavageLaoch <SavageLaoch@users.noreply.github.com>
2018-02-09 18:56:16 +0100

    Ejercicio 4

---
+++ Practica 1/Ejercicio4b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include	<sys/types.h>
+#include	<sys/wait.h>
+#include <unistd.h>
+
+#define NUM_PROC 6
+int main (void)
+{
+  int pid;
+  int i;
+  for (i=0; i <= NUM_PROC; i++){
+      if (i % 2 == 0) {
+          if ((pid=fork()) <0 ){
+              printf("Error al emplear fork\n");
+              exit(EXIT_FAILURE);
+            }else if (pid == 0){
+              printf("HIJO  %d\n",i);
+            }else{
+              printf("PADRE %d \n",i);
+            }
+          }
+        }
+        wait();
+    exit(EXIT_SUCCESS);
+}
