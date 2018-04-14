a0a3c7772911087fef230e8b73135c16746c7e23
mikism98 <miguelangelsm98@hotmail.com>
2018-04-14 21:11:32 +0200

    5 casi acabado

--- Practica3/ejercicio5.c
+++ Practica3/ejercicio5.c
@@ -20,18 +20,16 @@ int sem_id;
 
 typedef struct _Mensaje{
 	long id; /*Campo obligatorio a long que identifica el tipo de mensaje*/
-	char *contenido; /*Informacion a transmitir en el mensaje*/
+	char contenido[16]; /*Informacion a transmitir en el mensaje*/
 	int ultimo;
 }mensaje;
 
 int main(int argc, char *argv[]){
 	key_t clave;
-	int msqid,pid,i;
+	int msqid,pid1,pid2,i;
 	mensaje msg;
 	FILE *f;
-	char *buffer;
-
-	printf("Vamo a calmarno\n");
+	char buffer[16];
 
 	/*Comprobamos los parametros*/
 	if (argc < 3){
@@ -45,64 +43,61 @@ int main(int argc, char *argv[]){
 		perror("Error al obtener clave para cola mensajes\n");
 		exit(EXIT_FAILURE);
 	}
-	printf("Clave obtenida\n");
 
 	/*Creamos la cola de mensajes*/
 	msqid = msgget (clave, 0600 | IPC_CREAT);
 	if (msqid == -1){
-		perror("Error al obtener identificador para cola mensajes");
+		perror("Error al obtener identificador para cola mensajes\n");
 		return(0);
 	}
-	printf("Cola creada\n");
 
 	/*Con 2 forks creamos dos hijos*/
-	pid = fork();
-	if(pid < 0){
+	pid1 = fork();
+	if(pid1 < 0){
     	printf("No se ha podido crear el proceso hijo 1\n");
     	exit(EXIT_SUCCESS);
 	}
-	if(pid == 0){
-	printf("Soy el proceso A");
+	if(pid1 == 0){
+	printf("Soy el proceso A\n");
 	/*Aqui va lo que hace el proceso A*/
 		/*Leemos del fichero*/
-		f=fopen(argv[1],"r");
-		buffer = (char*)malloc(16*sizeof(char));
-		while (fgets(buffer,16,f)!=NULL){ /*Esto esta fatal*/
-			printf("%s",buffer);			
+		f=fopen(argv[1],"r");	
+		while (fgets(buffer,17,f)!=NULL){ /*Mientras que hay fichero*/
 			/*Mandamos el mensaje*/
 			msg.id=1; /*1 significa que aun no esta traducido, se lo manda al proceso B*/
 			strcpy(msg.contenido,buffer);
-			msg.ultimo=0;
+			msg.ultimo=0;			
 			msgsnd (msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long),IPC_NOWAIT);
+			printf("Enviado mensaje tipo %ld = %s \n", msg.id,msg.contenido);
 		}
 		fclose(f);
 		/*Aviso al proceso B de que he acabado mandando un mensaje con el valor de ultimo a 1*/
 		msg.id=1; 
 		msg.ultimo=1;
 		msgsnd (msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long),IPC_NOWAIT);
+		printf("Enviado mensaje de fin A\n");
 		exit(EXIT_SUCCESS);
 	}
-	pid = fork();
-	if(pid < 0){
+	pid2 = fork();
+	if(pid2 < 0){
     	printf("No se ha podido crear el proceso hijo 2\n");
     	exit(EXIT_SUCCESS);
 	}
-	if (pid==0){
-	printf("Soy el proceso B");
+	if (pid2==0){
+	printf("Soy el proceso B\n");
 	/*Aqui va lo que hace el proceso B*/
 		while (1){ /* Mientras que el proceso A no ha acabado */
 			/* Recibimos un mensaje */
 			msgrcv (msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long), 1, 0); /* Leemos solo los mensajes tipo 1 */
 			if (msg.ultimo==1){
-				printf("Este es el ultimo mensaje");
 				/*Aviso al proceso C de que he acabado*/
 				msg.id=2; 
 				msg.ultimo=1;
 				msgsnd (msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long),IPC_NOWAIT);
+				printf("Enviado mensaje de fin B\n");
 				exit(EXIT_SUCCESS);
 			}
-			printf("Recibido mensaje tipo %ld \n", msg.id);
-			printf("Mensaje = %s \n", msg.contenido);
+			printf("Recibido mensaje tipo %ld = %s \n", msg.id,msg.contenido);
 			/* Modificamos el mensaje */
 			for(i=0;i<sizeof(msg.contenido);i++){
 				msg.contenido[i]=msg.contenido[i]+1;
@@ -110,37 +105,25 @@ int main(int argc, char *argv[]){
 			/* Mandamos el mensaje */
 			msg.id=2; /*2 significa que ya esta traducido, se lo manda al proceso C*/
 			msgsnd (msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long),IPC_NOWAIT);
+			printf("Enviado mensaje tipo %ld = %s \n", msg.id,msg.contenido);
 		}
 		exit(EXIT_SUCCESS);
 	}
-	printf("Soy el proceso C");
+	printf("Soy el proceso C\n");
 	/*Aqui va lo que hace el proceso C*/
 	f=fopen(argv[2],"w");
 	while(1){/* Mientras que el procesoBA no ha acabado */
 		/* Recibimos un mensaje */
 		msgrcv (msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long), 2, 0); /* Leemos solo los mensajes tipo 2 */
 		if (msg.ultimo==1){
-			printf("YA SE HA TERMINADO TODO");
+			printf("YA HA TERMINADO TODO\n");
+			fclose(f);
 			exit(EXIT_SUCCESS);
 		}
-		printf("Recibido mensaje tipo %ld \n", msg.id);
-		printf("Mensaje = %s \n", msg.contenido);
+		printf("Recibido mensaje tipo %ld = %s\n", msg.id, msg.contenido);
 		/*Imprimimos el mensaje en el fichero*/
 		fprintf(f,"%s",msg.contenido);
 	}
 	fclose(f);
 	exit(EXIT_SUCCESS);
-
-
-
-
-
-
-
-
-
-
-
-
-	return 0;
 }
